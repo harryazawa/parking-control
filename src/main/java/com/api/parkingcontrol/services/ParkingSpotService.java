0363be9391bb3db920cd65d1d99274ec6f39cd44
Victor Amorim (Harry) <victorlbamorim@gmail.com>
2022-06-04 14:55:59 -0300

    added controllers, models, repositories, services

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+// possível criar interface para ser implementada
+// a uma outra classe em caso de mudar implementação
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+}
